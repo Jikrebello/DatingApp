140abf557d804dfb350be326cb2c07fe4b1438ac
James Rebello <Jikrebello.za@gmail.com>
2022-09-25 13:38:13 +0200

    Register, Log In/Out via client

--- client/src/app/navbar/navbar.component.ts
+++ client/src/app/navbar/navbar.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
 import { AccountService } from '../_services/account.service';
+import { User } from '../_models/user';
 
 @Component({
   selector: 'app-navbar',
@@ -9,19 +11,14 @@ import { AccountService } from '../_services/account.service';
 export class NavbarComponent implements OnInit {
   model: any = {};
 
-  loggedIn: boolean;
+  constructor(public accountService: AccountService) {}
 
-  constructor(private accountService: AccountService) {}
-
-  ngOnInit(): void {
-    this.getCurrentUser();
-  }
+  ngOnInit(): void {}
 
   login() {
     this.accountService.login(this.model).subscribe(
       (response) => {
         console.log(response);
-        this.loggedIn = true;
       },
       (error) => {
         console.log(error);
@@ -31,17 +28,5 @@ export class NavbarComponent implements OnInit {
 
   logOut() {
     this.accountService.logOut();
-    this.loggedIn = false;
-  }
-
-  getCurrentUser() {
-    this.accountService.currentUser$.subscribe(
-      (user) => {
-        this.loggedIn = !!user;
-      },
-      (error) => {
-        console.log(error);
-      }
-    );
   }
 }
